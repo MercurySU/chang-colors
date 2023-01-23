ba2e80fd6a665a933efc56207cbea210e3244f60
Suzana <suzana@adxspace.com.br>
2023-01-23 10:24:29 -0300

    chang colors in JS CSS HTML

---
+++ Curso.js
@@ -0,0 +1,24 @@
+function amarelo() {
+	document.getElementById("quadrado").style.background="yellow";
+}
+function azul() {
+	document.getElementById("quadrado").style.background="blue";
+}
+function cinza() {
+	document.getElementById("quadrado").style.background="gray";
+}
+function vermelho() {
+	document.getElementById("quadrado").style.background="red";
+}
+function verde() {
+	document.getElementById("quadrado").style.background="green";
+}
+function rosa() {
+	document.getElementById("quadrado").style.background="pink";
+}
+function laranja() {
+	document.getElementById("quadrado").style.background="orange";
+}
+function roxo() {
+	document.getElementById("quadrado").style.background="purple";
+}
